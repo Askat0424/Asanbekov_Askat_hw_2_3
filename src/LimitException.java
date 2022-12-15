dd9eb80a74ce32cb77da9da59b43b4183c98f193
Askat0424 <askatasanbekov0424@gmail.com>
2022-12-15 11:30:49 +0600

    Initial commit

---
+++ src/LimitException.java
@@ -0,0 +1,11 @@
+import javax.naming.LimitExceededException;
+public class LimitException extends Exception {
+    private double remainingAmount;
+    public LimitException(String message, double remainingAmount) {
+        super(message);
+        this.remainingAmount = remainingAmount;
+    }
+    public double getRemainingAmount() {
+        return remainingAmount;
+    }
+}
